cc651c02cfd2e3f40fd6fdacd648c280dd8d975a
Ion Mincu <ion.mincu@zitec.com>
2015-02-20 01:29:54 -0800

    Changed settings. Set the no sound timeout to 60s and browser wait load time to 10s.

--- ZitecSoundResetService/Settings.Designer.cs
+++ ZitecSoundResetService/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace ZitecSoundResetService {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("120000")]
+        [global::System.Configuration.DefaultSettingValueAttribute("60000")]
         public int RestartBrowserTimeout {
             get {
                 return ((int)(this["RestartBrowserTimeout"]));
@@ -37,7 +37,7 @@ namespace ZitecSoundResetService {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("30000")]
+        [global::System.Configuration.DefaultSettingValueAttribute("10000")]
         public int BrowserWaitLoadTime {
             get {
                 return ((int)(this["BrowserWaitLoadTime"]));
